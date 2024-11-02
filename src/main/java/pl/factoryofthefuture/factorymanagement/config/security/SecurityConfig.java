b4c0a5f8fe5b3dee3445e4f497cfc8217d94f535
gruchh <kubol2@gmail.com>
2024-11-02 22:30:35 +0100

    Spring security arrangement

---
+++ src/main/java/pl/factoryofthefuture/factorymanagement/config/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package pl.factoryofthefuture.factorymanagement.config.security;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((authorize) -> {
+                    authorize.requestMatchers("/h2-console/**").permitAll();
+                    authorize.anyRequest().authenticated();
+                }).httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+
+}
