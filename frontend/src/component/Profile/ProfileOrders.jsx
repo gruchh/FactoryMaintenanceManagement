a33265e23ca4586049ccb030c3758805e490b889
gruchh <kubol2@gmail.com>
2024-12-19 18:17:32 +0100

    UserProfile card implementation

--- frontend/src/component/Profile/ProfileOrders.jsx
+++ frontend/src/component/Profile/ProfileOrders.jsx
@@ -1,6 +1,9 @@
 const ProfileOrders = () => {
   return (
-    <div>Orders</div>
+    <div className="flex items-center flex-col">
+      <h1 className="text-xl text-center py-7 font-semibold">My orders</h1>
+      <div className="space-y-5 w-full  lg:w-1/2"></div>
+    </div>
   )}
 
 export default ProfileOrders
--- frontend/src/component/Profile/UserProfile.jsx
+++ frontend/src/component/Profile/UserProfile.jsx
@@ -1,6 +1,18 @@
+import Face6Icon from "@mui/icons-material/Face6";
+import { Button } from "@mui/material";
+
 const UserProfile = () => {
+  const handleLogout = () => {};
   return (
-    <div>UserProfile</div>
-  )
+    <div className="min-h-[80vh] flex flex-col justify-center items-center text-center">
+      <div className="flex flex-col items-center justify-center">
+        <Face6Icon sx={{ fontSize: "15rem;" }} />
+        <div className="py-5 text-2xl font-semibold">gruchh</div>
+        <Button onClick={() => handleLogout} sx={{ margin: "2rem 0rem" }}>
+          Logout
+        </Button>
+      </div>
+    </div>
+  );
 };
-export default UserProfile
+export default UserProfile;
